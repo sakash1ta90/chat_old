6caeba62bb00d6f18c3add3a088ef009f836e1d3
sakashita motoki <sakashita_motoki@cyberbuzz.com>
2018-09-27 17:35:55 +0900

    別に囲ってる必要はなかったので削除

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
-$(() => {
-    const getArgs = ['chat.php', {}, data => $('#result').html(data)];
+const getArgs = ['chat.php', {}, data => $('#result').html(data)];
 
-    // 一定間隔でログをリロードする
-    const reloadLog = () => {
-        getArgs[1] = {
-            user: $('#user').val(),
-            mode: 1, // 読み込み
-            type: 'html'
-        };
-        $.get(...getArgs);
-
-        // 最下部にスクロールさせる
-        $('#talkField').animate({scrollTop: $('#end').offset().top}, 'fast');
-
-        // ポーリング
-        setTimeout(reloadLog, 5000); // リロード時間はここで調整
+// 一定間隔でログをリロードする
+const reloadLog = () => {
+    getArgs[1] = {
+        user: $('#user').val(),
+        mode: 1, // 読み込み
+        type: 'html'
     };
+    $.get(...getArgs);
 
-    // 投稿ボタンクリック
-    $('#greet').on('click', () => {
-        getArgs[1] = {
-            user: $('#user').val(),
-            message: $('#message').val(),
-            mode: 0, // 書き込み
-            type: 'html'
-        };
-        if (!getArgs[1].user || !getArgs[1].message) {
-            return;
-        }
-        $.get(...getArgs);
-        $('#message').val('');
-    });
+    // 最下部にスクロールさせる
+    $('#talkField').animate({scrollTop: $('#end').offset().top}, 'fast');
 
-    reloadLog();
+    // ポーリング
+    setTimeout(reloadLog, 5000); // リロード時間
+};
+
+// 投稿ボタンクリック
+$('#greet').on('click', () => {
+    getArgs[1] = {
+        user: $('#user').val(),
+        message: $('#message').val(),
+        mode: 0, // 書き込み
+        type: 'html'
+    };
+    if (!getArgs[1].user || !getArgs[1].message) {
+        return;
+    }
+    $.get(...getArgs);
+    $('#message').val('');
 });
+reloadLog();
