3718cdd9cfa74b74e87edfb82d8f057b66640e16
sakashita motoki <sakashita_motoki@cyberbuzz.com>
2018-09-27 16:30:15 +0900

    余計な呼び出しを削除して関数を削減

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 $(() => {
     const getArgs = ['chat.php', {}, data => $('#result').html(data)];
 
+    // 一定間隔でログをリロードする
+    const reloadLog = () => {
+        getArgs[1] = {
+            user: $('#user').val(),
+            mode: 1, // 読み込み
+            type: 'html'
+        };
+        $.get(...getArgs);
+
+        // 最下部にスクロールさせる
+        $('#talkField').animate({scrollTop: $('#end').offset().top}, 'fast');
+
+        // ポーリング
+        setTimeout(() => {
+            reloadLog();
+        }, 5000); // リロード時間はここで調整
+    };
+
     // 投稿ボタンクリック
     $('#greet').on('click', () => {
         getArgs[1] = {
@@ -16,25 +34,5 @@ $(() => {
         $('#message').val('');
     });
 
-    // 最下部にスクロールさせる
-    const scrollToBottom = () => $('#talkField').animate({scrollTop: $('#end').offset().top}, 'fast');
-
-    // 一定間隔でログをリロードする
-    const reloadLog = () => {
-        getArgs[1] = {
-            user: $('#user').val(),
-            mode: 1, // 読み込み
-            type: 'html'
-        };
-        // ログリロード処理
-        $.get(...getArgs);
-
-        // ポーリング
-        setTimeout(() => {
-            reloadLog();
-            scrollToBottom();
-        }, 5000); // リロード時間はここで調整
-    };
     reloadLog();
-    scrollToBottom();
 });
