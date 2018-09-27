c2253c6bb2f54701649f555a8a87cb428bdb5761
sakashita motoki <sakashita_motoki@cyberbuzz.com>
2018-09-27 16:16:54 +0900

    魔改造#2

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
 $(() => {
-    const args = ['chat.php', {}, data => $('#result').html(data)];
+    const getArgs = ['chat.php', {}, data => $('#result').html(data)];
+
     // 投稿ボタンクリック
     $('#greet').on('click', () => {
-        if (!$('#user').val() || !$('#message').val()) {
-            return;
-        }
-        args[1] = {
+        getArgs[1] = {
             user: $('#user').val(),
             message: $('#message').val(),
             mode: 0, // 書き込み
             type: 'html'
         };
-        $.get(...args);
+        if (!getArgs[1].user || !getArgs[1].message) {
+            return;
+        }
+        $.get(...getArgs);
         $('#message').val('');
     });
 
+    // 最下部にスクロールさせる
+    const scrollToBottom = () => $('#talkField').animate({scrollTop: $('#end').offset().top}, 'fast');
+
     // 一定間隔でログをリロードする
-    const logAll = () => {
-        args[1] = {
+    const reloadLog = () => {
+        getArgs[1] = {
             user: $('#user').val(),
             mode: 1, // 読み込み
             type: 'html'
         };
         // ログリロード処理
-        $.get(...args);
+        $.get(...getArgs);
 
         // ポーリング
         setTimeout(() => {
-            logAll();
+            reloadLog();
+            scrollToBottom();
         }, 5000); // リロード時間はここで調整
     };
-    logAll();
+    reloadLog();
+    scrollToBottom();
 });
